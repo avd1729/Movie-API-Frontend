f9f81ecc64bb14a7aea3eb29df78f86c45675845
avd <aravind19112003@gmail.com>
2023-11-28 21:13:33 +0530

    express

--- movie-api/src/api/axiosConfig.js
+++ movie-api/src/api/axiosConfig.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
-//axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-//axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept';
-export default axios.create({
-    baseURL: 'https://66c3-121-200-50-94.ngrok-free.app/',
-    headers: {"ngrok-skip-browser-warning": "true"}
+
+const instance = axios.create({
+  baseURL: 'https://4b43-121-200-50-94.ngrok-free.app/',
+  headers: {
+    'Content-Type': 'application/json',
+    'ngrok-skip-browser-warning': 'true',
+    'Access-Control-Allow-Origin': '*',
+    // Add other necessary headers
+  },
+  withCredentials: true,
 });
+
+export default instance;
