4f85830aade541b994d9ae2a8b076d223cb5e0f4
avd <aravind19112003@gmail.com>
2023-11-29 11:51:48 +0530

    cors - error

--- movie-api/src/api/axiosConfig.js
+++ movie-api/src/api/axiosConfig.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://4b43-121-200-50-94.ngrok-free.app/',
+  baseURL: 'https://2e9a-2401-4900-7b95-4686-604b-35b8-ebfb-eaa0.ngrok-free.app',
   headers: {
     'Content-Type': 'application/json',
     'ngrok-skip-browser-warning': 'true',
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': 'true',
     // Add other necessary headers
   },
   withCredentials: true,
